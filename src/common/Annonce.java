13d4ca26d7e37552bf83c639468a22f33965e524
Etienne MARAIS <etiennemarais@maiste.fr>
2020-10-26 10:21:34 +0100

    Annonce update

--- src/common/Annonce.java
+++ src/common/Annonce.java
@@ -10,6 +10,9 @@ public class Annonce {
 	private String prix;
 
 	public Annonce(String user, String domaine, String titre, String descriptif, String prix) {
+		if(user == null || domaine == null || titre == null || descriptif == null || prix == null) {
+			throw new IllegalArgumentException("Annonce must be full");
+		}
 		this.domaine = domaine;
 		this.user = user;
 		this.titre = titre;
@@ -64,5 +67,42 @@ public class Annonce {
 
 	public String getId() {
 		return this.idAnnonce;
+	}
+
+	public String[] toStringArgs() {
+		String[] args = new String[5];
+		args[0] = getId();
+		args[1] = getDomaine();
+		args[2] = getTitre();
+		args[3] = getDescriptif();
+		args[4] = getPrix();
+		return args;
+	}
+
+	public static Annonce fromStringArgs(String user, String []args) {
+		if(args.length != 4) {
+			return null;
+		} else {
+			try {
+				return new Annonce(user, args[0], args[1], args[2], args[3]);
+			} catch(IllegalArgumentException e) {
+				return null;
+			}
+		}
+	}
+
+	private String filterString(String str) {
+		return (str.equals("null")) ? null : str;
+	}
+
+	public boolean updateWithArgs(String[] args) {
+		if(args.length != 5) {
+			return false;
+		}
+		setDomaine(filterString(args[1]));
+		setTitre(filterString(args[2]));
+		setDescriptif(filterString(args[3]));
+		setPrix(filterString(args[4]));
+		return true;
 	}
 }
