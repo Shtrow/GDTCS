2b16a3c9aabb1cf9a91aa79f65c251a8950d58c9
Benjamin Viau <benjamin_viau@yahoo.com>
2020-11-15 15:35:57 +0100

    okok

--- src/test/PromptBox.java
+++ src/test/PromptBox.java
@@ -1,7 +1,9 @@
 package test;
 
 import com.googlecode.lanterna.gui2.BorderLayout;
+import com.googlecode.lanterna.gui2.Interactable;
 import com.googlecode.lanterna.gui2.TextBox;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 
 import java.util.function.Consumer;
@@ -18,6 +20,23 @@ public class PromptBox extends TextBox {
                 }
                 return true;
             });
+//            this.handleKeyStroke(new KeyStroke(KeyType.Enter));
 
         }
+
+    @Override
+    public synchronized Result handleKeyStroke(KeyStroke keyStroke) {
+            if(keyStroke.getKeyType() == KeyType.Enter){
+                return Result.HANDLED;
+            }
+            return super.handleKeyStroke(keyStroke);
+    }
+
+    @Override
+    protected void afterLeaveFocus(FocusChangeDirection direction, Interactable nextInFocus) {
+        super.afterLeaveFocus(direction, nextInFocus);
+        System.out.println("SDF");
+        this.takeFocus();
+    }
+
 }
